8146e931350e3d4200fe73dcb7de676a1ae7343e
StefanSko <Stefan.sko@gmail.com>
2024-03-26 17:58:21 +0100

    initial tests with mixtral and modal

---
+++ modal-llama-cpp.py
@@ -0,0 +1,58 @@
+from modal import Image, Stub, method, gpu, enter
+
+GPU_CONFIG = gpu.A100(memory=40, count=1)
+MODEL_REPOS = "TheBloke/Mixtral-8x7B-v0.1-GGUF"
+MODEL_FILENAME = "mixtral-8x7b-v0.1.Q5_K_M.gguf"
+MODEL_DIR = "model/"
+
+
+def download_model():
+    from huggingface_hub import hf_hub_download
+    hf_hub_download(repo_id=MODEL_REPOS, filename=MODEL_FILENAME, local_dir=MODEL_DIR)
+
+image = (
+    Image.from_registry("nvidia/cuda:12.1.0-cudnn8-devel-ubuntu22.04", add_python="3.11")
+    .apt_install("build-essential")
+    .pip_install(
+        "huggingface_hub",
+        "sse_starlette",
+    ).run_commands("CMAKE_ARGS=\"-DLLAMA_CUBLAS=on\" FORCE_CMAKE=1 pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir", gpu=GPU_CONFIG)
+    .run_function(download_model)
+)
+
+stub = Stub("example-llama-cpp-mixtral", image=image)
+
+@stub.cls(gpu=GPU_CONFIG,
+          allow_concurrent_inputs=10,
+          container_idle_timeout=60 * 10,
+          timeout=60 * 60,
+          image=image)
+class Model:
+
+    @enter()
+    def startup(self):
+        from llama_cpp import Llama
+        self.llama = Llama(MODEL_DIR + "/" + MODEL_FILENAME, n_ctx=4096, n_gpu_layers=-1, verbose=True)
+
+
+    @method()
+    def generate(self, question: str, context: str = ""):
+
+        formatted_question = f"{context}<s>[INST]{question}[/INST]"
+
+        return self.llama(
+            formatted_question,
+            temperature=0.1,
+            max_tokens=-1,
+            stop=["</s>"],
+            stream=False,
+            echo=True
+        )
+
+@stub.local_entrypoint()
+def main():
+    print(
+        Model().generate.remote(
+            "What is the capital of Germany?"
+        )
+    )
